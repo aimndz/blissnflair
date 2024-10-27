a888e7283ca5198832416f462e2f97861ec68009
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-28 01:49:37 +0800

    Fix: User data retrieval on page reload

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,14 +1,33 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { AccountProfile, UserContextType } from "../types/account";
+import { getUserProfile } from "../services/utilsApi";
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 function UserProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<AccountProfile | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const userProfile = await getUserProfile();
+        if (userProfile?.user) {
+          setUser(userProfile.user);
+        }
+      } catch (error) {
+        console.error("Failed to fetch user profile:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserProfile();
+  }, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      {children}
+      {loading ? <div>Loading...</div> : children}
     </UserContext.Provider>
   );
 }
