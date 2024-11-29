0424dd6bc630d57a07a3bc5dd57421fc4df6d129
Endex <marclainjames@gmail.com>
2024-11-29 09:43:23 +0800

    eto yung bago

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,24 +3,24 @@ function Home() {
   return (
     <div className="flex h-screen items-center justify-center px-4">
   {/* Content Wrapper */}
-  <div className="w-full max-w-sm text-center">
+  <div className="w-full text-center">
     {/* Logo */}
     <div className="mb-6 flex justify-center">
       <Logo />
     </div>
 
     {/* Title */}
-    <h1 className="text-2xl font-semibold text-gray-800 mb-4">
-      Welcome to <span className="text-primary-200">Bliss and Fair</span>
+    <h1 className="text-4xl font-semibold text-gray-800 mb-4">
+      Welcome to <span className="text-primary-200">Bliss and Flair</span>
     </h1>
 
     {/* Subtitle */}
-    <p className="text-sm text-gray-600 mb-8">
+    <p className="text-sm text-gray-600 mb-8 max-w-lg mx-auto">
       Your ultimate destination for unforgettable events, a homely staycation, and ideal commercial spaces!
     </p>
 
     {/* Login Button */}
-    <div className="flex space-x-4">
+    <div className="flex max-w-sm space-x-4 content-center mx-auto">
     <a href="/login"
       className="block w-full rounded-full bg-primary-100 px-6 py-3 font-bold text-secondary-900 hover:bg-primary-200 transition-colors">
       Login
