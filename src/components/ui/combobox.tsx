96b78e5ee0df37b82e9a5c78ff6ab99bd826b59a
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-12-02 00:30:53 +0800

    Chore: Modify ui components

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./command";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+
+interface Item {
+  value: string;
+  label: string;
+}
+
+interface ComboboxProps {
+  items: Item[];
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+function Combobox({ items, label, value, onChange }: ComboboxProps) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[240px] justify-between border-secondary-600 bg-secondary-100"
+          >
+            {value ? items.find((item) => item.value === value)?.label : label}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[240px] p-0">
+          <Command>
+            <CommandList>
+              <CommandEmpty>No item found.</CommandEmpty>
+              <CommandGroup>
+                {items.map((item) => (
+                  <CommandItem
+                    key={item.value}
+                    value={item.value}
+                    onSelect={(selectedValue) => {
+                      onChange(selectedValue === value ? "" : selectedValue); // Toggle value or set it
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === item.value ? "opacity-100" : "opacity-0",
+                      )}
+                    />
+                    {item.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
+
+export default Combobox;
