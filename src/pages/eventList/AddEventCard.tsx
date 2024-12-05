5b2a14d6ed2670cacd0fcb3df9193dd01c45bf71
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-12-05 21:57:29 +0800

    Feat: Change filter to combobox

--- src/pages/eventList/AddEventCard.tsx
+++ src/pages/eventList/AddEventCard.tsx
@@ -5,7 +5,7 @@ function AddEventCard() {
   return (
     <Link
       to={"/dashboard/create/select-venue"}
-      className="flex h-48 items-center justify-center rounded-lg bg-secondary-300"
+      className="flex h-48 items-center justify-center rounded-lg bg-secondary-300 transition-all duration-200 ease-in-out hover:bg-secondary-600"
     >
       <Plus className="text-secondary-800" />
     </Link>
--- src/pages/eventList/UserEventListContent.tsx
+++ src/pages/eventList/UserEventListContent.tsx
@@ -1,5 +1,3 @@
-import { TabsContent } from "@radix-ui/react-tabs";
-import { Tabs, TabsList, TabsTrigger } from "../../components/ui/tabs";
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getAllEvents } from "../../services/eventApi";
@@ -7,15 +5,62 @@ import { Event } from "../../types/event";
 import EventCard from "./EventCard";
 import NoEventsMessage from "./NoEventsMessage";
 import AddEventCard from "./AddEventCard";
+import Combobox from "../../components/ui/combobox";
+
+const eventStatus = [
+  {
+    value: "approved",
+    label: "Approved",
+  },
+  {
+    value: "pending",
+    label: "Pending",
+  },
+  {
+    value: "rejected",
+    label: "Rejected",
+  },
+  {
+    value: "cancelled",
+    label: "Cancelled",
+  },
+  {
+    value: "completed",
+    label: "Completed",
+  },
+];
 
 function UserEventListContent() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [events, setEvents] = useState<Event[]>([]);
 
-  const filter = searchParams.get("filter") || "scheduled";
+  const filter = searchParams.get("filter") || "approved";
 
-  const pendingEvents = events.filter((event) => event.status === "PENDING");
-  const scheduledEvents = events.filter((event) => event.status === "APPROVED");
+  const currentFilterLabel =
+    eventStatus.find((status) => status.value === filter)?.label || "Approved";
+
+  const filteredEvents = events.filter((event) => {
+    switch (filter) {
+      case "pending":
+        return event.status === "PENDING";
+      case "approved":
+        return event.status === "APPROVED";
+      case "cancelled":
+        return event.status === "CANCELLED";
+      case "completed":
+        return event.status === "COMPLETED";
+      case "rejected":
+        return event.status === "REJECTED";
+      default:
+        return event.status === "APPROVED";
+    }
+  });
+
+  useEffect(() => {
+    if (!searchParams.get("filter")) {
+      setSearchParams({ filter: "approved" }, { replace: true });
+    }
+  }, [searchParams, setSearchParams]);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -32,46 +77,35 @@ function UserEventListContent() {
     fetchEvents();
   }, []);
 
-  const handleTabChange = (value: string) => {
+  const handleFilterChange = (value: string) => {
     setSearchParams({ filter: value });
   };
 
   return (
     <div>
-      <Tabs value={filter} onValueChange={handleTabChange} className="w-full">
-        <TabsList>
-          <TabsTrigger value="scheduled">Scheduled</TabsTrigger>
-          <TabsTrigger value="pending">Pending</TabsTrigger>
-        </TabsList>
+      <div className="flex flex-col">
+        <p className="text-sm font-medium">Event status</p>
+        <Combobox
+          items={eventStatus}
+          label=""
+          value={filter}
+          onChange={handleFilterChange}
+        />
+        {/* <span className="flex aspect-square h-8 items-center justify-center rounded-full border border-secondary-600 bg-secondary-100 text-xl font-medium">
+          {filteredEvents.length}
+        </span> */}
+      </div>
 
-        {/* Scheduled events */}
-        <TabsContent value="scheduled">
-          {scheduledEvents.length > 0 ? (
-            <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4">
-              {scheduledEvents.map((event) => (
-                <EventCard key={event.id} event={event} />
-              ))}
-              <AddEventCard />
-            </div>
-          ) : (
-            <NoEventsMessage message="No scheduled events" />
-          )}
-        </TabsContent>
-
-        {/* Pending events */}
-        <TabsContent value="pending">
-          {pendingEvents.length > 0 ? (
-            <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4">
-              {pendingEvents.map((event) => (
-                <EventCard key={event.id} event={event} />
-              ))}
-              <AddEventCard />
-            </div>
-          ) : (
-            <NoEventsMessage message="No pending events" />
-          )}
-        </TabsContent>
-      </Tabs>
+      {filteredEvents.length > 0 ? (
+        <div className="mt-3 grid grid-cols-1 gap-3 md:grid-cols-3 lg:grid-cols-4">
+          {filteredEvents.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+          <AddEventCard />
+        </div>
+      ) : (
+        <NoEventsMessage message={`No ${filter} events`} />
+      )}
     </div>
   );
 }
