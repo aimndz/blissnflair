ec67c58f74c37190081b0cdbeb363e0547a030fe
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-12-12 01:09:37 +0800

    UI: Modify event details styles

--- src/pages/eventDetails/Event.tsx
+++ src/pages/eventDetails/Event.tsx
@@ -77,51 +77,59 @@ function Event() {
           <span>Event is currently under review</span>
         </div>
       )}
-      <div className="flex h-60 w-full items-center justify-center rounded-lg border border-solid border-secondary-600 bg-secondary-300">
-        <Image className="text-secondary-100" size={40} />
-      </div>
-      <div className="mt-5 gap-3">
-        <span className="text-xs text-secondary-800">{event?.category}</span>
-        <h2 className="text-2xl font-bold">{event?.title}</h2>
-      </div>
-      <div className="mt-3 flex flex-wrap items-center gap-3">
-        <Calendar className="text-secondary-800" size={20} />
-        <span className="mr-10">
-          {event?.date && format(new Date(event.date), "EEE, MMM d, yyyy")}
-        </span>
-        <span>
-          {event?.startTime && format(parseISO(event.startTime), "h:mm a")} -{" "}
-          {event?.endTime && format(parseISO(event.endTime), "h:mm a")}
-        </span>
-      </div>
-      <p className="mt-3 flex gap-3">
-        <MapPin className="text-secondary-800" size={20} />
-        <span>{eventSpace?.name}</span>
-      </p>
-      <div className="mt-8">
-        <h3 className="font-medium">Additional Services:</h3>
-        <div className="flex flex-wrap gap-3">
-          {event?.additionalServices?.map((service: string) => (
-            <span
-              key={service}
-              className="rounded-lg border border-solid border-secondary-600 bg-secondary-300 px-3 py-1"
-            >
-              {service}
-            </span>
-          ))}
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
+        <div className="col-span-1 flex aspect-square items-center justify-center rounded-lg border border-solid border-secondary-600 bg-secondary-300">
+          <Image className="text-secondary-100" size={40} />
         </div>
-      </div>
-      <div className="mt-8">
-        <h3 className="font-medium">Description:</h3>
-        <p>{event?.description}</p>
-      </div>
-      <div className="mt-8">
-        {event?.additionalNotes?.trim() !== "" && (
+        <div className="col-span-2">
           <div>
-            <h3 className="font-medium">Note:</h3>
-            <p>{event?.additionalNotes}</p>
+            <span className="text-xs text-secondary-800">
+              {event?.category}
+            </span>
+            <h2 className="text-2xl font-bold">{event?.title}</h2>
           </div>
-        )}
+          <div className="mt-3 flex flex-wrap items-center gap-3">
+            <Calendar className="text-secondary-800" size={20} />
+            <span>
+              {event?.date && format(new Date(event.date), "EEE, MMM d, yyyy")}
+            </span>
+            <span>
+              {event?.startTime && format(parseISO(event.startTime), "h:mm a")}{" "}
+              - {event?.endTime && format(parseISO(event.endTime), "h:mm a")}
+            </span>
+          </div>
+          <p className="mt-3 flex gap-3">
+            <MapPin className="text-secondary-800" size={20} />
+            <span>{eventSpace?.name}</span>
+          </p>
+          {event?.additionalServices.length > 0 && (
+            <div className="mt-8">
+              <h3 className="font-medium">Additional Services:</h3>
+              <div className="flex flex-wrap gap-3">
+                {event?.additionalServices?.map((service: string) => (
+                  <span
+                    key={service}
+                    className="rounded-lg border border-solid border-secondary-600 bg-secondary-300 px-3 py-1"
+                  >
+                    {service}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className="mt-8">
+            <h3 className="font-medium">Description:</h3>
+            <p>{event?.description}</p>
+          </div>
+          <div className="mt-8">
+            {event?.additionalNotes?.trim() !== "" && (
+              <div>
+                <h3 className="font-medium">Note:</h3>
+                <p>{event?.additionalNotes}</p>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
