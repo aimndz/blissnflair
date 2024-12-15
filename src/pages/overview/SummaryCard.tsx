1ee9e7969f1a291ad0560681cf547239e3f8aec8
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-12-15 22:29:39 +0800

    Feat: Add chart description

--- src/pages/overview/SummaryCard.tsx
+++ src/pages/overview/SummaryCard.tsx
@@ -46,38 +46,43 @@ function SummaryCard({
     <Card>
       <CardContent className="flex items-center gap-3 px-3 py-5 text-3xl font-semibold">
         <p className="w-3/5">{value}</p>
-        <ChartContainer config={chartConfig} className="w-2/5">
-          <LineChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              left: 12,
-              right: 12,
-              top: 12,
-              bottom: 12,
-            }}
-          >
-            {/* <CartesianGrid vertical={false} /> */}
-            {/* <XAxis
-              dataKey="month"
-              tickLine={false}
-              axisLine={false}
-              tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
-            /> */}
-            {/* <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            /> */}
-            <Line
-              dataKey={dataKey}
-              type="natural"
-              stroke={chartColor}
-              strokeWidth={2}
-              dot={false}
-            />
-          </LineChart>
-        </ChartContainer>
+        <div className="w-2/5">
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+                top: 12,
+                bottom: 12,
+              }}
+            >
+              {/* <CartesianGrid vertical={false} /> */}
+              {/* <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              /> */}
+              {/* <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              /> */}
+              <Line
+                dataKey={dataKey}
+                type="natural"
+                stroke={chartColor}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+          <p className="mt-3 text-center text-xs font-normal text-secondary-800">
+            Past 7 Days
+          </p>
+        </div>
       </CardContent>
       <CardHeader className="px-3 pb-3 pt-0">
         <CardTitle className="truncate">{title}</CardTitle>
