a0741fac5e2e168642bc93d79128dfcccd8a7931
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-12-24 15:21:17 +0800

    UI: Add external catering styles

--- src/pages/create/ExternalCatering.tsx
+++ src/pages/create/ExternalCatering.tsx
@@ -1,5 +1,22 @@
+import { Card } from "../../components/ui/card";
+
 function ExternalCatering() {
-  return <div className="mt-24">External Catering</div>;
+  return (
+    <Card className="px-3 py-10 text-center">
+      <div className="mx-auto w-full max-w-xl">
+        <h1 className="mb-3 text-2xl font-semibold">External Catering</h1>
+        <p>
+          You have selected an{" "}
+          <span className="font-semibold">external catering service</span>.
+        </p>
+        <p>
+          A <span className="font-semibold">corkage fee</span> will apply, and
+          the amount will depend on the services.
+        </p>
+        <p className="mt-3">Please contact us for details.</p>
+      </div>
+    </Card>
+  );
 }
 
 export default ExternalCatering;
